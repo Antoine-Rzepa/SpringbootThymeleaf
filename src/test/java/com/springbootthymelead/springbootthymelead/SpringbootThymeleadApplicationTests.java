490d9e325275f7cdcaac12dfc2ed7033eb9d61c6
ANtoine-Rzepa <antoine.rzepa@le-campus-numerique.fr>
2021-10-19 10:37:27 +0200

    first commit

---
+++ src/test/java/com/springbootthymelead/springbootthymelead/SpringbootThymeleadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootthymelead.springbootthymelead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
